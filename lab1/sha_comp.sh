0ad39d30de3c3c5bd9a1a401cb232a09cd2ac468
idatewang <idatewang@gmail.com>
2023-02-06 13:41:13 -0600

    updating sha_comps.sh

--- lab1/sha_comp.sh
+++ lab1/sha_comp.sh
@@ -1,7 +1,15 @@
 while [[ true ]]; do
 	./test3
-	dm 0xFFFC0000 | sha256sum > 0000_sha
-	dm 0xFFFC2000 | sha256sum > 2000_sha
-	echo "Compared..."
-	diff 0000_sha 2000_sha
+	dm 0xFFFC2000 | grep -o "=.*" | sha256sum > 2000_sha
+	dm 0xFFFC0000 | grep -o "=.*" | sha256sum > 0000_sha
+	echo "Compared..............."
+	DIFF=$(diff 0000_sha 2000_sha)
+	if [ "$DIFF" ] 
+	then
+		diff 0000_sha 2000_sha
+	    echo "Not the same."
+	else
+		diff 0000_sha 2000_sha
+		echo "Files are the same."
+	fi
 done
